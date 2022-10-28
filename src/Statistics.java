e73aa33b453d7359660cc9dd20afc4415ea5e905
AnJiYeong <memarcis2@naver.com>
2022-10-28 15:19:15 +0900

    221028

---
+++ src/Statistics.java
@@ -0,0 +1,9 @@
+import java.sql.Statement;
+
+public class Statistics {
+    public void statistics(Statement statement) {
+        // 통계
+        System.out.println("통계"); // 확인용
+        // 통계 출력
+    }
+}
